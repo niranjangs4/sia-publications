ceb6ab936308efea129d1f53c0e2a4fbe8cdad2f
Niranjan <niranjan4@outlook.in>
2020-07-04 14:31:33 +0530

    Updates

---
+++ precache-manifest.91c7fc0a7dff7718ffcf99693dc2eaee.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f118458bc3bc6e9a2fa3",
+    "url": "/sia-publications/static/js/app.5dead2f9.chunk.js"
+  },
+  {
+    "revision": "8b95c329a89a63d245df",
+    "url": "/sia-publications/static/js/runtime~app.06f1bef8.js"
+  },
+  {
+    "revision": "9680261b27315f9cdca4",
+    "url": "/sia-publications/static/js/2.0399e02e.chunk.js"
+  },
+  {
+    "revision": "869a5640020fbae87edb71d6a7799f67",
+    "url": "/sia-publications/static/media/min_icon.869a5640.svg"
+  },
+  {
+    "revision": "f31165b33e675cde842394d33ce6a3c3",
+    "url": "/sia-publications/static/media/min_icon_h.f31165b3.svg"
+  },
+  {
+    "revision": "5f8af7f7fb2d9398b0945ce40d12367d",
+    "url": "/sia-publications/static/media/select-all.5f8af7f7.svg"
+  },
+  {
+    "revision": "ea3e358939ef919dada8799801affc47",
+    "url": "/sia-publications/static/media/low-vision.ea3e3589.svg"
+  },
+  {
+    "revision": "adac6b64cb830be39f74a44869a11cdb",
+    "url": "/sia-publications/static/media/close_icon.adac6b64.svg"
+  },
+  {
+    "revision": "d4e3c3b62081be73cabf956839c2adf0",
+    "url": "/sia-publications/static/media/close_icon_h.d4e3c3b6.svg"
+  },
+  {
+    "revision": "35e50454063115252905aa69ca783cca",
+    "url": "/sia-publications/static/media/fulls_icon.35e50454.svg"
+  },
+  {
+    "revision": "adcdea46ec0f04ad44760a1aa818b989",
+    "url": "/sia-publications/static/media/fulls_icon_h.adcdea46.svg"
+  },
+  {
+    "revision": "5d99b0f645abff402e7cc3ed89c55fb6",
+    "url": "/sia-publications/static/media/mins_icon.5d99b0f6.svg"
+  },
+  {
+    "revision": "8702a88b807af465bef4eeaafc73d3c9",
+    "url": "/sia-publications/static/media/mins_icon_h.8702a88b.svg"
+  },
+  {
+    "revision": "1a04db83644e7dc0967dcf5e78160f9e",
+    "url": "/sia-publications/static/media/max_icon.1a04db83.svg"
+  },
+  {
+    "revision": "96ab2cadc9c42f28b175e6b38b18fade",
+    "url": "/sia-publications/static/media/max_icon_h.96ab2cad.svg"
+  },
+  {
+    "revision": "9378c98e32f3ea2f0f60c442ee1ffb28",
+    "url": "/sia-publications/static/media/formula.9378c98e.svg"
+  },
+  {
+    "revision": "4b09e769738e3256af6616af5672790e",
+    "url": "/sia-publications/static/media/subscript.4b09e769.svg"
+  },
+  {
+    "revision": "812aba03d2d12d2ca13255d3038a6010",
+    "url": "/sia-publications/static/media/superscript.812aba03.svg"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/sia-publications/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/sia-publications/expo-service-worker.js"
+  },
+  {
+    "revision": "9a734b9bf81bf06d9578f22ae07bedad",
+    "url": "/sia-publications/index.html"
+  },
+  {
+    "revision": "b543eaae48b497bb509d3450429a85f1",
+    "url": "/sia-publications/manifest.json"
+  },
+  {
+    "revision": "6166c7a28cb94109b13672ca1306c19e",
+    "url": "/sia-publications/register-service-worker.js"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sia-publications/precache-manifest.4b0149170988d1fb0edc4cad1576b248.js"
+  "/sia-publications/precache-manifest.91c7fc0a7dff7718ffcf99693dc2eaee.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.skipWaiting();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +26,12 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sia-publications/index.html"), {
+workbox.routing.registerNavigationRoute("\\sia-publications\\index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
+
+workbox.routing.registerRoute(/^https?.*/, workbox.strategies.networkFirst({ "cacheName":"offlineCache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"purgeOnQuotaError":false})] }), 'GET');
