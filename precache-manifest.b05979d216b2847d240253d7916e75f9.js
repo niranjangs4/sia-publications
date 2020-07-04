6477c8b31a0b0882c96a4a33310d59c5d03ba202
Niranjan <niranjan4@outlook.in>
2020-07-04 14:40:59 +0530

    Updates

---
+++ precache-manifest.b05979d216b2847d240253d7916e75f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18edfea44253ffadd3c45d637c9ca220",
+    "url": "/sia-publications/index.html"
+  },
+  {
+    "revision": "21f44d1134c3f79545ce",
+    "url": "/sia-publications/static/js/2.f3928c5b.chunk.js"
+  },
+  {
+    "revision": "d00ed1e90d8521a08f4a9de5be1f8afc",
+    "url": "/sia-publications/static/js/2.f3928c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c81b8a7af01744aad7",
+    "url": "/sia-publications/static/js/main.deb85869.chunk.js"
+  },
+  {
+    "revision": "1e0834571c411023002d",
+    "url": "/sia-publications/static/js/runtime-main.b20c50fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,14 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sia-publications/precache-manifest.91c7fc0a7dff7718ffcf99693dc2eaee.js"
+  "/sia-publications/precache-manifest.b05979d216b2847d240253d7916e75f9.js"
 );
 
-workbox.skipWaiting();
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -26,12 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("\\sia-publications\\index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sia-publications/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
-
-workbox.routing.registerRoute(/^https?.*/, workbox.strategies.networkFirst({ "cacheName":"offlineCache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"purgeOnQuotaError":false})] }), 'GET');
